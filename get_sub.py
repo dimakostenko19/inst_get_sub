8abae1e33ff329371dbf2a34d36b0609cf87822a
track01 <dkostenko44@gmail.com>
2024-06-28 11:50:03 +0300

    3 commit

--- get_sub.py
+++ get_sub.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
-ttt
+
 import time
 import pickle
 
